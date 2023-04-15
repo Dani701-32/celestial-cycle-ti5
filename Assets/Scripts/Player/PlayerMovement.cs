ef7b42e66186627d8175b1c193848c79ed1fe4c5
Daniel Henrique <85520325+Dani701-32@users.noreply.github.com>
2023-04-15 18:04:14 -0300

    Add rotation to the movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
+    public float speed, rotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +18,13 @@ public class PlayerMovement : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
         movement.Normalize();
-        transform.Translate(movement * speed * Time.deltaTime);
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+
+        //Rotate player
+        if (movement != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotation * Time.deltaTime);
+        }
     }
 }
