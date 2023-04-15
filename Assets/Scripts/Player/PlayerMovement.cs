501b15d350b1a898c563859f4537dfa27f2e3be3
Daniel Henrique <85520325+Dani701-32@users.noreply.github.com>
2023-04-15 17:56:46 -0300

    Creating basic movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
+        movement.Normalize();
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+}
