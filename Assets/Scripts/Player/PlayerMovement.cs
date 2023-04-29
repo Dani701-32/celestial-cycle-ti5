1b930f72349998666b7ecf5a64bd43843b03b2ef
Daniel Henrique <85520325+Dani701-32@users.noreply.github.com>
2023-04-29 19:51:46 -0300

    placeholder anim
    Using placeholder animations and models for better visualization of the game

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed, rotation;
+    public float speed, maxSpeed, rotation, jumpSpeed;
+    private float ySpeed, stepOffset;
     private CharacterController characterController;
+    [SerializeField] private Animator animator;
     [SerializeField] private Transform cameraTransform;
+    bool isRunning = false;
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        stepOffset = characterController.stepOffset;
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -18,13 +23,43 @@ public class PlayerMovement : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
+        isRunning = (Input.GetKey(KeyCode.LeftShift));
 
+        //Animation
+        animator.SetBool("isWalking", (verticalInput != 0 || horizontalInput != 0));
+        animator.SetBool("isRunning", isRunning);
+        
+
+        //Movment
+        speed = (isRunning) ? maxSpeed : speed;
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
         float magnitude = Mathf.Clamp01(movement.magnitude) * speed;
         movement = Quaternion.AngleAxis(cameraTransform.rotation.eulerAngles.y, Vector3.up) * movement;
         movement.Normalize();
 
-        characterController.SimpleMove(movement * magnitude);
+        //Jump
+                    
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+        if (characterController.isGrounded)
+        {
+            ySpeed = -0.5f;
+            characterController.stepOffset = stepOffset;
+            if (Input.GetButtonDown("Jump"))
+            {
+                
+                ySpeed = jumpSpeed;
+            }
+            // animator.SetBool("IsJumping", false);
+        }
+        else
+        {
+            // animator.SetBool("IsJumping", true);
+            characterController.stepOffset = 0;
+        }
+
+        Vector3 velocity = movement * magnitude;
+        velocity.y = ySpeed;
+        characterController.Move(velocity * Time.deltaTime);
 
         //Rotate player
         if (movement != Vector3.zero)
