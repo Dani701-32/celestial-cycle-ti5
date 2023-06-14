aac3e102a8efc02137bed7a7348ea82211407ccd
VitorHugoDV <vitoravelinosantos2001@gmail.com>
2023-06-14 18:19:17 -0300

    Sistema de Save Finalizado
    O sistema de save está funcional com relação ao sistema de dia e noite e já tem a fogueira pra onde salvar.

--- Assets/Scripts/Controllers/MenuController.cs
+++ Assets/Scripts/Controllers/MenuController.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.UI;
 using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
---
+++ Assets/Scripts/SaveSystem/SaveObject.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveObject : MonoBehaviour
+{
+    private SavingLoading savingLoading;
+
+    [SerializeField]
+    private bool isSave;
+
+    public GameObject messageObject;
+
+    void Awake()
+    {
+        savingLoading = FindObjectOfType<SavingLoading>().GetComponent<SavingLoading>();
+        ControlStateMessage(messageObject, false);
+    }
+
+    public void ControlStateMessage(GameObject obj, bool state)
+    {
+        obj.SetActive(state);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            isSave = true;
+            ControlPlayerActionsOnSave(isSave);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isSave = false;
+        ControlPlayerActionsOnSave(isSave);
+    }
+
+    public void CallSaveUI()
+    {
+        savingLoading.Save();
+        isSave = false;
+        ControlPlayerActionsOnSave(isSave);
+    }
+
+    public void ExitSaveUI()
+    {
+        isSave = false;
+        ControlPlayerActionsOnSave(isSave);
+    }
+
+    public void ControlPlayerActionsOnSave(bool state)
+    {
+        if(state)
+        {
+            ControlStateMessage(messageObject, true);
+            Cursor.lockState = CursorLockMode.None;
+            GameController.gameController.player.playerMovement.enabled = false;
+            GameController.gameController.StopCamera();
+        }
+        else
+        {
+            ControlStateMessage(messageObject, false);
+            Cursor.lockState = CursorLockMode.Locked;
+            GameController.gameController.ReleaseCamera();
+            GameController.gameController.player.playerMovement.enabled = true;
+        }
+    }
+}
