d1e20220497f8d70b278a96a435d4c6dc8653901
Daniel Henrique <85520325+Dani701-32@users.noreply.github.com>
2023-11-29 16:00:27 -0300

    fix rest of the details

--- Assets/Scripts/Inventory/ItemTypes/Consumables/ConsumableFullMoon.cs
+++ Assets/Scripts/Inventory/ItemTypes/Consumables/ConsumableFullMoon.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class ConsumableFullMoon : Consumable
 {
+    public float lifeRestored; 
     public override void Use()
     {
         Player player = GameController.gameController.player;
+        player.RestoreLife(lifeRestored);
+
     }
 }
